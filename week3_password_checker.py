b7b0f1fa523d55cb5fb19f0320f33f59ee9d8b35
jackcox343 <145562567+jackcox343@users.noreply.github.com>
2024-02-15 10:16:40 +1000

    Add authenticate_password

--- week3_password_checker.py
+++ week3_password_checker.py
@@ -10,3 +10,13 @@ def main():
     pass
 
 
+
+
+
+def authenticate_password():
+    if user_password == correct_password:
+        print("Authentication successful! You may proceed.")
+        return True
+    else:
+        print("Authentication failed! Incorrect password.")
+        return False
